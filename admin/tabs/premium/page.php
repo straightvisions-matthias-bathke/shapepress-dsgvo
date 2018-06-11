dd2fdd1fe1fd6cc2baf738fce14e20b0e94dcbf1
Matthias Reuter <matthias-reuter@straightvisions.com>
2018-06-11 17:33:46 +0200

    init #3

--- admin/tabs/premium/page.php
+++
@@ -1,1 +0,0 @@
-<h1>Premium Optionen</h1>
--- public/shortcodes/terms-conditions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function SPDSGVOTermsConditionsShortcode(){
-    return apply_filters('the_content', SPDSGVOSettings::get('terms_conditions'));
-}
-
-add_shortcode('terms_conditions', 'SPDSGVOTermsConditionsShortcode');
